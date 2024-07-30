973ef83195fa8f6be9bdc3bc9f04b4a6b6cc9fe3
jakkapet2k <jakkapet.l@gmail.com>
2024-07-31 05:37:11 +0700

    eyebrowpencil,eyeliner

---
+++ eyebrowpencil.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ศูนย์กลางเครื่องสำอาง">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <title>ศูนย์กลางเครื่องสำอาง</title>
+    <link href="style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <header class="w-100 header px-5">
+        <nav class="navbar navbar-expand-lg container">
+            <div class="d-flex flex-wrap w-100 justify-content-between align-items-center">
+                <a href="#">
+                    <img src="img/logo.png" alt="Logo">
+                </a>
+                <ul class="nav nav-pills">
+                    <li class="nav-item mx-4"><a href="#" class="fs-4 fw-normal text-BF6159 text-decoration-none" aria-current="page">Home</a></li>
+                    <li class="nav-item mx-4"><a href="#" class="fs-4 fw-normal text-BF6159 text-decoration-none">Products</a></li>
+                    <li class="nav-item mx-4"><a href="#" class="fs-4 fw-normal text-BF6159 text-decoration-none">About</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-3 ">
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body">
+                    <h5 class="card-title"style="color: #BF6159;">MILLE 6D EYEBROW PENCIL WATERPROOF</h5>
+                    <img src="img/MILLE6DEYEBROWPENCILWATERPROOF.png" alt="BEAUTILAB A2P High-Performing Sleek Black Liner" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">MILLE 2 IN 1 BROW PENCIL MASCARA WATERPROOF</h5>
+                <img src="img/MILLE2IN1BROWPENCILMASCARAWATERPROOF.png" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Sasi Brow-to-beAuto Pencil Eyebrow</h5>
+                <img src="img/SasiBrow-to-beAutoPencilEyebrow.png" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Sasi Girls Can Define Auto Eyebrow </h5>
+                <img src="img/SasiGirlsCanDefineAutoEyebrow.jpg" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Sasi Pretty Easy Perfect Eyebrow Pencil 02 Natural Brown</h5>
+                <img src="img/SasiPrettyEasyPerfectEyebrowPencil02NaturalBrown.jpg" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Browit ProMakeup Flat Eyebrow Pencil</h5>
+                <img src="img/BrowitProMakeupFlatEyebrowPencil.jpg" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Browit Ultra Fine Duo Eyebrow Pencil & Mascara</h5>
+                <img src="img/BrowitUltraFineDuoEyebrowPencil&Mascara.jpg" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+            
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Browit Pro Slim Brow Pencil</h5>
+                <img src="img/BrowitProSlimBrowPencil.jpeg" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Browit Sharpen Brow Pencil</h5>
+                <img src="img/BrowitSharpenBrowPencil.jpeg" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+            <div class="card border-#BF6159  mb-3 mt-5 mx-3" style="max-width: 16rem; min-height: 20rem; border: 2px solid #BF6159; ">
+                <div class="card-body ">
+                <h5 class="card-title"style="color: #BF6159;">Browit Trio Tips Eyebrow Pencil</h5>
+                <img src="img/BrowitTrioTipsEyebrowPencil.jpeg" alt="Description" style="max-width: 100%; height: auto;">
+                </div>
+            </div>
+
+
+            
+        </div>
+        
+    </div>
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
